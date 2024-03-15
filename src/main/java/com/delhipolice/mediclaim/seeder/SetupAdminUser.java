3d6ddd3223c0c77f7330c6dc4e89419a44b13ca9
Vikas Mahato <vikasmahato0@gmail.com>
2024-03-15 13:22:20 +0530

    Add users

--- src/main/java/com/delhipolice/mediclaim/seeder/SetupAdminUser.java
+++ src/main/java/com/delhipolice/mediclaim/seeder/SetupAdminUser.java
@@ -18,11 +18,14 @@ public class SetupAdminUser implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         try {
-            userService.loadUserByUsername("vikasmahato0@gmail.com");
+            userService.loadUserByUsername("hagcrimebranch1@gmail.com");
+
         } catch (Exception e) {
             String encodedPassword = passwordEncoder.encode("adminPassword");
-            userService.createAdminUser("vikasmahato0@gmail.com", encodedPassword);
-            userService.createAdminUser("hagcrimebranch@gmail.com", encodedPassword);
+            userService.createAdminUser("hagcrimebranch1@gmail.com", encodedPassword);
+            userService.createAdminUser("hagcrimebranch2@gmail.com", encodedPassword);
+            //userService.createAdminUser("vikasmahato0@gmail.com", encodedPassword);
+            //userService.createAdminUser("hagcrimebranch@gmail.com", encodedPassword);
         }
     }
 }
