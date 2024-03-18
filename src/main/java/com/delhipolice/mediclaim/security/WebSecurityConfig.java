106b8886c64f0c52acee80c254fcb754ff5c0a88
Vikas Mahato <vikasmahato0@gmail.com>
2024-03-18 20:39:01 +0530

    Update cors configuration

--- src/main/java/com/delhipolice/mediclaim/security/WebSecurityConfig.java
+++ src/main/java/com/delhipolice/mediclaim/security/WebSecurityConfig.java
@@ -16,8 +16,9 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.Collections;
 
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Autowired
@@ -54,7 +55,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://3.6.246.208", "http://hagsoftware.in", "https://hagsoftware.in", "http://localhost"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
